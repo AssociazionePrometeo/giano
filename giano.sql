0c582b77e1b29a0131dd898a39a43a032280abef
Francesco D'Aluisio <fdaluisio@fedoraproject.org>
2016-09-26 15:38:46 +0200

    add booking table for reservation

--- giano.sql
+++ giano.sql
@@ -1,190 +1,250 @@
--- phpMyAdmin SQL Dump
--- version 4.4.15.7
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Creato il: Set 14, 2016 alle 00:10
--- Versione del server: 5.5.50-MariaDB
--- Versione PHP: 5.5.38
+-- MySQL Workbench Forward Engineering
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema giano
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `giano` ;
+
+-- -----------------------------------------------------
+-- Schema giano
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `giano` DEFAULT CHARACTER SET utf8 ;
+USE `giano` ;
+
+-- -----------------------------------------------------
+-- Table `giano`.`type_device`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giano`.`type_device` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- -----------------------------------------------------
+-- Table `giano`.`devices`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giano`.`devices` (
+  `id` INT(255) NOT NULL AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
+  `active` INT(11) NOT NULL DEFAULT '0',
+  `type` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_devices_1_idx` (`type` ASC),
+  CONSTRAINT `fk_type_device`
+    FOREIGN KEY (`type`)
+    REFERENCES `giano`.`type_device` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
---
--- Database: `giano`
---
 
-drop database if exists giano ; 
-create database if not exists giano;
-use giano;
+-- -----------------------------------------------------
+-- Table `giano`.`log`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giano`.`log` (
+  `id` INT(25) NOT NULL AUTO_INCREMENT,
+  `userid` INT(225) NOT NULL,
+  `cardcode` TEXT NOT NULL,
+  `date_log` DATETIME NOT NULL,
+  `devicelog` INT(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8;
 
--- --------------------------------------------------------
 
---
--- Struttura della tabella `devices`
---
+-- -----------------------------------------------------
+-- Table `giano`.`permissions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giano`.`permissions` (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `insert_devices` TINYINT(4) NOT NULL DEFAULT 0,
+  `insert_tags` TINYINT(4) NOT NULL DEFAULT 0,
+  `insert_users` TINYINT(4) NOT NULL DEFAULT 0,
+  `delete_devices` TINYINT(4) NOT NULL DEFAULT 0,
+  `delete_tags` TINYINT(4) NOT NULL DEFAULT 0,
+  `delete_users` TINYINT(4) NOT NULL DEFAULT 0,
+  `insert_reservation` TINYINT(4) NOT NULL DEFAULT 0,
+  `delete_reservation` TINYINT(4) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS `devices` (
-  `id` int(255) NOT NULL,
-  `name` text NOT NULL,
-  `active` int(11) NOT NULL DEFAULT '0',
-  `type` enum('0','1','2','3') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
---
--- Dump dei dati per la tabella `devices`
---
+-- -----------------------------------------------------
+-- Table `giano`.`system`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giano`.`system` (
+  `site name` MEDIUMTEXT NOT NULL,
+  `site url` TEXT NOT NULL,
+  `status` ENUM('0', '1') NOT NULL DEFAULT '0')
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-INSERT INTO `devices` (`id`, `name`, `active`, `type`) VALUES
-(1, 'porta 1', 1, '1'),
-(2, 'porta 2', 1, '1');
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `giano`.`type_user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giano`.`type_user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `level` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
---
--- Struttura della tabella `log`
---
 
-CREATE TABLE IF NOT EXISTS `log` (
-  `id` int(25) NOT NULL,
-  `userid` int(225) NOT NULL,
-  `cardcode` text NOT NULL,
-  `date_log` datetime NOT NULL,
-  `device` int(25) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+-- -----------------------------------------------------
+-- Table `giano`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giano`.`users` (
+  `userid` INT(25) NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(25) NOT NULL DEFAULT '',
+  `email_address` VARCHAR(25) NULL DEFAULT '',
+  `username` VARCHAR(25) NOT NULL DEFAULT '',
+  `PASSWORD` VARCHAR(255) NOT NULL DEFAULT '',
+  `info` TEXT NULL,
+  `user_level` INT NOT NULL DEFAULT '3',
+  `signup_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_login` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `activated` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`userid`),
+  INDEX `fk_user_level_idx` (`user_level` ASC),
+  CONSTRAINT `fk_user_level`
+    FOREIGN KEY (`user_level`)
+    REFERENCES `giano`.`type_user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 30
+DEFAULT CHARACTER SET = latin1
+COMMENT = 'Membership Information';
 
---
--- Dump dei dati per la tabella `log`
---
 
-INSERT INTO `log` (`id`, `userid`, `cardcode`, `date_log`, `device`) VALUES
-(1, 1, 'cardcode1', '2016-09-11 07:25:12', 1),
-(2, 20, 'cardcode2', '2016-09-10 06:22:34', 1),
-(3, 1, 'cardcode1', '2016-09-13 07:25:12', 2),
-(4, 20, 'cardcode2', '2016-09-11 15:40:34', 2);
+-- -----------------------------------------------------
+-- Table `giano`.`tags`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giano`.`tags` (
+  `id` INT(25) NOT NULL AUTO_INCREMENT,
+  `cardcode` TEXT NOT NULL,
+  `userid` INT NULL,
+  `status` INT(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_userid_tags_idx` (`userid` ASC),
+  CONSTRAINT `fk_userid_tags`
+    FOREIGN KEY (`userid`)
+    REFERENCES `giano`.`users` (`userid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
--- --------------------------------------------------------
 
---
--- Struttura della tabella `system`
---
+-- -----------------------------------------------------
+-- Table `giano`.`reservation`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `giano`.`reservation` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `deviceid` INT NOT NULL,
+  `userid` INT NOT NULL,
+  `from_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `to_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`, `deviceid`, `userid`),
+  INDEX `fk_reservation_userid_idx` (`userid` ASC),
+  INDEX `fk_reservation_deviceid_idx` (`deviceid` ASC),
+  CONSTRAINT `fk_reservation_userid`
+    FOREIGN KEY (`userid`)
+    REFERENCES `giano`.`users` (`userid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_reservation_deviceid`
+    FOREIGN KEY (`deviceid`)
+    REFERENCES `giano`.`devices` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `system` (
-  `site name` mediumtext NOT NULL,
-  `site url` text NOT NULL,
-  `status` enum('0','1') NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Data for table `giano`.`type_device`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `giano`;
+INSERT INTO `giano`.`type_device` (`id`, `name`) VALUES (1, 'Porta');
 
---
--- Struttura della tabella `tags`
---
+COMMIT;
 
-CREATE TABLE IF NOT EXISTS `tags` (
-  `id` int(25) NOT NULL,
-  `cardcode` text NOT NULL,
-  `userid` int(255) NOT NULL,
-  `status` int(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
---
--- Dump dei dati per la tabella `tags`
---
+-- -----------------------------------------------------
+-- Data for table `giano`.`devices`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `giano`;
+INSERT INTO `giano`.`devices` (`id`, `name`, `active`, `type`) VALUES (1, 'Ingresso', 1, 1);
+INSERT INTO `giano`.`devices` (`id`, `name`, `active`, `type`) VALUES (2, 'Sala Riunioni', 1, 1);
 
-INSERT INTO `tags` (`id`, `cardcode`, `userid`, `status`) VALUES
-(1, 'cardcode1', 1, 0),
-(2, 'cardcode2', 20, 0);
+COMMIT;
 
--- --------------------------------------------------------
 
---
--- Struttura della tabella `users`
---
+-- -----------------------------------------------------
+-- Data for table `giano`.`permissions`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `giano`;
+INSERT INTO `giano`.`permissions` (`id`, `name`, `insert_devices`, `insert_tags`, `insert_users`, `delete_devices`, `delete_tags`, `delete_users`, `insert_reservation`, `delete_reservation`) VALUES (1, 'administration', 1, 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO `giano`.`permissions` (`id`, `name`, `insert_devices`, `insert_tags`, `insert_users`, `delete_devices`, `delete_tags`, `delete_users`, `insert_reservation`, `delete_reservation`) VALUES (2, 'manager', 1, 1, 1, 0, 0, 0, 1, 1);
+INSERT INTO `giano`.`permissions` (`id`, `name`, `insert_devices`, `insert_tags`, `insert_users`, `delete_devices`, `delete_tags`, `delete_users`, `insert_reservation`, `delete_reservation`) VALUES (3, 'user', 0, 0, 0, 0, 0, 0, 1, 1);
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `userid` int(25) NOT NULL,
-  `first_name` varchar(25) NOT NULL DEFAULT '',
-  `last_name` varchar(25) NOT NULL DEFAULT '',
-  `email_address` varchar(25) NOT NULL DEFAULT '',
-  `username` varchar(25) NOT NULL DEFAULT '',
-  `PASSWORD` varchar(255) NOT NULL DEFAULT '',
-  `decrypted_password` varchar(255) NOT NULL DEFAULT '',
-  `info` text NOT NULL,
-  `user_level` enum('-1','0','1','2','3') NOT NULL DEFAULT '-1',
-  `signup_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `end_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `last_login` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `activated` enum('0','1') NOT NULL DEFAULT '0'
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1 COMMENT='Membership Information';
+COMMIT;
 
---
--- Dump dei dati per la tabella `users`
---
 
-INSERT INTO `users` (`userid`, `first_name`, `last_name`, `email_address`, `username`, `PASSWORD`, `decrypted_password`, `info`, `user_level`, `signup_date`, `end_date`, `last_login`, `activated`) VALUES
-(1, '', '', '', 'admin', '6e6bc4e49dd477ebc98ef4046c067b5f', 'ciao', '', '0', '2006-04-06 15:56:58', '0000-00-00 00:00:00', '2016-09-13 22:00:31', '1'),
-(20, '', '', '', 'utente1', 'b88d6b04a9dc38860301f6bdd81e5ccd', 'utente1', 'poco privilegiato', '1', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2016-09-11 11:30:48', '1'),
-(21, 'ugo', 'prova', 'prova@ugo.com', 'utente2', 'ciao', '5f423b7772a80f77438407c8b78ff305', '', '1', '2016-09-12 05:22:26', '2017-08-16 15:32:35', '0000-00-00 00:00:00', '1');
+-- -----------------------------------------------------
+-- Data for table `giano`.`type_user`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `giano`;
+INSERT INTO `giano`.`type_user` (`id`, `level`) VALUES (1, 'administrator');
+INSERT INTO `giano`.`type_user` (`id`, `level`) VALUES (2, 'manager');
+INSERT INTO `giano`.`type_user` (`id`, `level`) VALUES (3, 'user');
 
---
--- Indici per le tabelle scaricate
---
+COMMIT;
 
---
--- Indici per le tabelle `devices`
---
-ALTER TABLE `devices`
-  ADD PRIMARY KEY (`id`);
 
---
--- Indici per le tabelle `log`
---
-ALTER TABLE `log`
-  ADD PRIMARY KEY (`id`);
+-- -----------------------------------------------------
+-- Data for table `giano`.`users`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `giano`;
+INSERT INTO `giano`.`users` (`userid`, `first_name`, `email_address`, `username`, `PASSWORD`, `info`, `user_level`, `signup_date`, `end_date`, `last_login`, `activated`) VALUES (1, 'admin', 'admin@fb7.it', 'admin', '6e6bc4e49dd477ebc98ef4046c067b5f', 'Fablab', 1, DEFAULT, DEFAULT, DEFAULT, 1);
+INSERT INTO `giano`.`users` (`userid`, `first_name`, `email_address`, `username`, `PASSWORD`, `info`, `user_level`, `signup_date`, `end_date`, `last_login`, `activated`) VALUES (2, 'manager', 'manager@fb7.it', 'manager', '6e6bc4e49dd477ebc98ef4046c067b5f', 'Fablab', 2, DEFAULT, DEFAULT, DEFAULT, 1);
+INSERT INTO `giano`.`users` (`userid`, `first_name`, `email_address`, `username`, `PASSWORD`, `info`, `user_level`, `signup_date`, `end_date`, `last_login`, `activated`) VALUES (3, 'user', 'user@fb7.it', 'user', '6e6bc4e49dd477ebc98ef4046c067b5f', 'Fablab', 3, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
 
---
--- Indici per le tabelle `tags`
---
-ALTER TABLE `tags`
-  ADD PRIMARY KEY (`id`);
+COMMIT;
 
---
--- Indici per le tabelle `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userid`);
 
---
--- AUTO_INCREMENT per le tabelle scaricate
---
+-- -----------------------------------------------------
+-- Data for table `giano`.`tags`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `giano`;
+INSERT INTO `giano`.`tags` (`id`, `cardcode`, `userid`, `status`) VALUES (1, '112314', 1, 1);
+INSERT INTO `giano`.`tags` (`id`, `cardcode`, `userid`, `status`) VALUES (2, '4324', 2, 1);
+INSERT INTO `giano`.`tags` (`id`, `cardcode`, `userid`, `status`) VALUES (3, '47beet', 3, 1);
 
---
--- AUTO_INCREMENT per la tabella `devices`
---
-ALTER TABLE `devices`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT per la tabella `log`
---
-ALTER TABLE `log`
-  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT per la tabella `tags`
---
-ALTER TABLE `tags`
-  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT per la tabella `users`
---
-ALTER TABLE `users`
-  MODIFY `userid` int(25) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
