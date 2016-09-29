03dcf803de7c8f96908f93741d70641e0d85d06e
Giuseppe Treccarichi <giutrec@gmail.com>
2016-09-30 00:50:58 +0200

    refused file

--- admin/_menu.php
+++ admin/_menu.php
@@ -25,16 +25,16 @@ include '../function/database.php';
                 <li class="dropdown"> <a href="#" class="dropdown-toggle" id="drop1" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Utenti<span class="caret"></span> </a>
                     <ul class="dropdown-menu" aria-labelledby="drop1">
                         <li><a href="list_user.php">Lista Utenti</a></li>
-                        <li><a href="create_user.php">Aggiungi Utente</a></li>
-                        <li role="separator" class="divider"></li>
                         <?php
                     $pdo = Database::connect();
                     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    $sql = 'SELECT permissions FROM permissions WHERE id=?';
+                    $sql = 'SELECT insert_users, permissions FROM permissions WHERE id=?';
                     $q = $pdo->prepare($sql);
                     $q->execute(array($_SESSION['user_level']));
                     $data = $q->fetch(PDO::FETCH_ASSOC);
-                        if($data['permissions'] == 1)  echo '<li><a href="list_profile.php">Gestione Profili</a></li>';
+                    if($data['insert_users'] == 1)  echo '<li><a href="create_user.php">Aggiungi Utente</a></li>';
+                    echo '<li role="separator" class="divider"></li>';
+                    if($data['permissions'] == 1)  echo '<li><a href="list_profile.php">Gestione Profili</a></li>';
                     Database::disconnect();
                         ?>
                         <li><a href="#"></a></li>
@@ -43,17 +43,32 @@ include '../function/database.php';
                 <li class="dropdown"> <a href="#" class="dropdown-toggle" id="drop2" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Devices<span class="caret"></span> </a>
                     <ul class="dropdown-menu" aria-labelledby="drop2">
                         <li><a href="list_device.php">Lista Devices</a></li>
-                        <li><a href="create_device.php">Aggiungi Devices</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
+                         <?php
+                    $pdo = Database::connect();
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $sql = 'SELECT insert_devices FROM permissions WHERE id=?';
+                    $q = $pdo->prepare($sql);
+                    $q->execute(array($_SESSION['user_level']));
+                    $data = $q->fetch(PDO::FETCH_ASSOC);
+                    if($data['insert_devices'] == 1)  echo '<li><a href="create_device.php">Aggiungi Devices</a></li>';
+                    Database::disconnect();
+                        ?>
+                     </ul>
                 </li>
                 <li class="dropdown"> <a href="#" class="dropdown-toggle" id="drop3" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Tags<span class="caret"></span> </a>
                     <ul class="dropdown-menu" aria-labelledby="drop3">
                         <li><a href="list_tag.php">Lista Tags</a></li>
-                        <li><a href="create_tag.php">Aggiungi Tags</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
+
+                        <?php
+                    $pdo = Database::connect();
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $sql = 'SELECT insert_tags FROM permissions WHERE id=?';
+                    $q = $pdo->prepare($sql);
+                    $q->execute(array($_SESSION['user_level']));
+                    $data = $q->fetch(PDO::FETCH_ASSOC);
+                    if($data['insert_tags'] == 1)  echo '<li><a href="create_tag.php">Aggiungi Tags</a></li>';
+                    Database::disconnect();
+                        ?>
                     </ul>
                 </li>
             </ul>
